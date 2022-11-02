ebe04f4a702822e9489b8cb0ddaa4ab0bf843882
Rodrigo Leitão <rodrigoncleitao@gmail.com>
2022-11-02 17:50:43 +0000

    Auto stash before merge of "master" and "origin/master"

--- prr/core/Plan.java
+++ prr/core/Plan.java
@@ -4,6 +4,9 @@ abstract public class Plan {
 
     public abstract double computeTextCost(Client c, TextCommunication communication);
 
+    
+
+
     public abstract double computeVoiceCost(Client c, VoiceCommunication communication);
 
     public abstract double computeVideoCost(Client c, VideoCommunication communication);
